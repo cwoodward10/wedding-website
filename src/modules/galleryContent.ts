668b57595678ca5ac71dfa662f80ac3f054aac42
woody-desktop <c.woodward10@gmail.com>
2024-12-10 17:21:19 -0600

    - update gallery images

---
+++ src/modules/galleryContent.ts
@@ -0,0 +1,150 @@
+import Image4 from '@assets/images/gallery/NatalieChris-4.jpg';
+import Image11 from '@assets/images/gallery/NatalieChris-11.jpg';
+import Image13 from '@assets/images/gallery/NatalieChris-13.jpg';
+import Image25 from '@assets/images/gallery/NatalieChris-25.jpg';
+import Image51 from '@assets/images/gallery/NatalieChris-51.jpg';
+import Image58 from '@assets/images/gallery/NatalieChris-58.jpg';
+import Image72 from '@assets/images/gallery/NatalieChris-72.jpg';
+import Image84 from '@assets/images/gallery/NatalieChris-84.jpg';
+import Image96 from '@assets/images/gallery/NatalieChris-96.jpg';
+import Image105 from '@assets/images/gallery/NatalieChris-105.jpg';
+import Image110 from '@assets/images/gallery/NatalieChris-110.jpg';
+import Image120 from '@assets/images/gallery/NatalieChris-120.jpg';
+import Image130 from '@assets/images/gallery/NatalieChris-130.jpg';
+import Image153 from '@assets/images/gallery/NatalieChris-153.jpg';
+import Image176 from '@assets/images/gallery/NatalieChris-176.jpg';
+import Image203 from '@assets/images/gallery/NatalieChris-203.jpg';
+import Image212 from '@assets/images/gallery/NatalieChris-212.jpg';
+import Image225 from '@assets/images/gallery/NatalieChris-225.jpg';
+import Image233 from '@assets/images/gallery/NatalieChris-233.jpg';
+import Image266 from '@assets/images/gallery/NatalieChris-266.jpg';
+
+export type GalleryImage = {
+    name: string;
+    src: ImageMetadata;
+    alt: string;
+    direction: "horizontal" | "vertical";
+}
+
+export const GalleryContent: GalleryImage[] = [
+    {
+        src: Image4,
+        name: 'image-4',
+        alt: "Natlie and Chris at Humboldt Park",
+        direction: 'vertical'
+    },
+    {
+        src: Image11,
+        name: 'image-11',
+        alt: "Natlie and Chris at Humboldt Park",
+        direction: 'vertical'
+    },
+    {
+        src: Image13,
+        name: 'image-13',
+        alt: "Natlie and Chris at Humboldt Park",
+        direction: 'horizontal'
+    },
+    {
+        src: Image25,
+        name: 'image-25',
+        alt: "Natlie and Chris at Humboldt Park",
+        direction: 'vertical'
+    },
+    {
+        src: Image51,
+        name: 'image-51',
+        alt: "Natlie and Chris at Humboldt Park",
+        direction: 'vertical'
+    },
+    {
+        src: Image58,
+        name: 'image-58',
+        alt: "Natlie and Chris at Humboldt Park",
+        direction: 'vertical'
+    },
+    {
+        src: Image72,
+        name: 'image-72',
+        alt: "Natlie and Chris at Humboldt Park",
+        direction: 'vertical'
+    },
+    {
+        src: Image84,
+        name: 'image-84',
+        alt: "Natlie and Chris at Humboldt Park",
+        direction: 'vertical'
+    },
+    {
+        src: Image96,
+        name: 'image-96',
+        alt: "Natlie and Chris at Humboldt Park",
+        direction: 'vertical'
+    },
+    {
+        src: Image105,
+        name: 'image-105',
+        alt: "Natlie and Chris at Humboldt Park",
+        direction: 'vertical'
+    },
+    {
+        src: Image110,
+        name: 'image-110',
+        alt: "Natlie and Chris at a the tennis courts",
+        direction: 'horizontal'
+    },
+    {
+        src: Image120,
+        name: 'image-120',
+        alt: "Natlie and Chris at a the tennis courts",
+        direction: 'vertical'
+    },
+    {
+        src: Image130,
+        name: 'image-130',
+        alt: "Natlie and Chris at a the tennis courts",
+        direction: 'vertical'
+    },
+    {
+        src: Image153,
+        name: 'image-153',
+        alt: "Natlie and Chris at a the tennis courts",
+        direction: 'vertical'
+    },
+    {
+        src: Image176,
+        name: 'image-176',
+        alt: "Natlie and Chris at a the tennis courts",
+        direction: 'vertical'
+    },
+    {
+        src: Image203,
+        name: 'image-203',
+        alt: "Natlie and Chris at a the tennis courts",
+        direction: 'vertical'
+    },
+    {
+        src: Image212,
+        name: 'image-212',
+        alt: "Natlie and Chris at a the tennis courts",
+        direction: 'horizontal'
+    },
+    {
+        src: Image225,
+        name: 'image-225',
+        alt: "Natlie and Chris at a the tennis courts",
+        direction: 'vertical'
+    },
+    {
+        src: Image233,
+        name: 'image-233',
+        alt: "Natlie and Chris at a the tennis courts",
+        direction: 'vertical'
+    },
+    {
+        src: Image266,
+        name: 'image-266',
+        alt: "Natlie and Chris at a the tennis courts",
+        direction: 'vertical'
+    },
+]
