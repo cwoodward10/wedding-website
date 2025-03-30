b70bb4f135ef771575cb5c36c9837eae05127f8b
Woody PopOs <c.woodward10@gmail.com>
2025-03-30 11:06:03 -0500

    - add accordion experience to travel page

---
+++ src/components/TravelAccomodations.svelte
@@ -0,0 +1,232 @@
+<script lang="ts">
+    import TheBean from '@assets/images/chicago/thebean.jpg';
+    import LincolnPark from '@assets/images/chicago/lincolnpark.jpg';
+    import WickerPark from '@assets/images/chicago/wickerpark.jpg';
+    import QuietYard from '@assets/images/chicago/quietyard.jpg';
+
+
+	import Accordion from './generic/Accordion.svelte';
+
+    import { flip } from 'svelte/animate';
+    import { cubicOut }  from 'svelte/easing';
+
+    type Area = {
+        name: string,
+        tag: string,
+        image: {
+            src: any,
+            alt: string
+        }
+        neighborhoods: Record<string, string>
+    }
+    const cityRegions: Area[] = [
+        {
+            name: 'Downtown',
+            tag: 'Sites, Shopping, and Tourism',
+            image: {
+                src: TheBean,
+                alt: 'The Bean. Photo by Antonio Gabola on Unsplash'
+            },
+            neighborhoods: {
+                'The Loop': 'The heart of Chicago, known for its iconic architecture and cultural landmarks.',
+                'River North': 'A vibrant area with art galleries, trendy restaurants, and nightlife.',
+                'Streeterville': 'Home to Navy Pier and stunning lakefront views.'
+            }
+        },
+        {
+            name: 'North Side',
+            tag: 'Upscale Dining and Proximity to Lake',
+            image: {
+                src: LincolnPark,
+                alt: 'Lincoln Park. Photo by Alex Sawyer on Unsplash'
+            },
+            neighborhoods: {
+                'Lakeview': 'A lively neighborhood with bars, restaurants, and the famous Wrigley Field.',
+                'Lincoln Park': 'Offers a mix of quiet streets, fine dining, and nightlife. It also has the beautiful Lincoln Park Zoo.',
+                'Andersonville': 'Known for its diverse community and eclectic mix of shops and eateries.'
+            }
+        },
+        {
+            name: 'Northwest Side',
+            tag: 'Community and Proximity to Venue',
+            image: {
+                src: QuietYard,
+                alt: 'Photo by Nesh Lonzoy on Unsplash'
+            },
+            neighborhoods: {
+                'North Center': 'A family-friendly area with parks and local breweries. (Closest to venue!)',
+                'Lincoln Park': 'Quiet neighborhood featuring charming boutiques, cafes, and a strong sense of community.',
+                'Albany Park': 'A culturally rich neighborhood with diverse, and really tasty, dining options. (Where we live now!)'
+            }
+        },
+        {
+            name: 'The West Side',
+            tag: 'A Mix of Trends and History',
+            image: {
+                src: WickerPark,
+                alt: 'Photo by Dominique Caron on Unsplash'
+            },
+            neighborhoods: {
+                'West Town': 'A gentrifying area with a mix of historic homes, local shops, and modern amenities.',
+                'Wicker Park': 'Known for its artistic vibe, trendy shops, and vibrant nightlife. (Where we lived before!)',
+                'Logan Square': 'Offers a blend of historic charm and contemporary culture. Hipsters love it here.'
+            }
+        }
+    ]
+    
+    const regionsComponents: (null | Accordion)[] = [ null, null, null, null];
+    
+    let openRegion: number = 0;
+    const handleToggle = (e: CustomEvent, index: number) => {
+        if (e.detail) {
+            regionsComponents.forEach((r: Accordion | null, i: number) => {
+                index !== i && r?.Close();
+            })
+            openRegion = index;
+        }
+    }
+</script>
+
+<div class="travel-accomodations">
+    <div class="accordions container">
+        {#each cityRegions as region, i}
+        <Accordion 
+            bind:this={regionsComponents[i]}
+            title={region.name} 
+            startOpen={i === 0}
+            preventCloseOnClick={true}
+            on:click={(e) => handleToggle(e, i)}
+        >
+            <div class="city-region">
+                <div>
+                    <p class="tag">Best for:</p>
+                    <p>{region.tag}</p>
+                </div>
+                <div>
+                    <p class="tag">Consider staying in:</p>
+                    <ul>
+                        {#each Object.keys(region.neighborhoods) as neighborhood }
+                        <li>
+                            <p class="title">{neighborhood}</p>
+                            <p>{region.neighborhoods[neighborhood]}</p>
+                        </li>
+                        {/each}
+                    </ul>
+                </div>
+            </div>
+        </Accordion>
+        {/each}
+    </div>
+
+    <div class="images container">
+        {#each cityRegions as region, i}
+        {@const image = region.image}
+        <div 
+            class="image" 
+            class:active={i === openRegion }
+        >
+            <img src={image.src.src} alt={image.alt}/>
+        </div>
+        {/each}
+    </div>
+</div>
+
+<style lang="scss">
+    @use '@styles/media.scss' as Media;
+
+    .travel-accomodations {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+
+        @include Media.at('medium') {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: var(--space-small)
+        }
+        @include Media.at('large') {
+            gap: var(--space-medium);
+        }
+
+        .accordions.container {
+            gap: var(--space-line);
+
+            :global(details:last-child) {
+                border-bottom: none;
+            }
+
+            :global(summary) {
+                font-size: 1.5rem;
+                color: var(--color-green);
+            }
+            :global(details:not([open]) summary) {
+                opacity: 40%;
+                color: var(--color-black)
+            }
+
+            .city-region {
+                display: flex;
+                flex-direction: column;
+                gap: var(--space-line);
+
+                padding: var(--space-line) 0;
+
+                &>div {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 0.25rem;
+
+                    ul {
+                        margin: 0;
+                    }
+                }
+            }
+        }
+
+        .images.container {
+            display: none;
+            content-visibility: hidden;
+
+            @include Media.at('medium') {
+                content-visibility: auto;
+                width: 100%;
+                height: fit-content;
+                overflow: hidden;
+
+                display: grid;
+                grid-template-columns: 1fr;
+                gap: var(--space-tiny);
+
+                box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.2);
+            }
+            @include Media.at('large') {
+                gap: var(--space-line);
+            }
+
+            .image {
+                width: 100%;
+                aspect-ratio: 5/7;
+                grid-area: 1/1;
+                overflow: hidden;
+
+                border-radius: 2px;
+
+                transition: opacity 500ms ease-out;
+
+                &:not(.active) {
+                    opacity: 0;
+                    pointer-events: none;
+                }
+
+                img {
+                    min-height: 100%;
+                    min-width: 100%;
+                    width: auto;
+                    height: auto;
+
+                    filter: grayscale(0.25);
+                }
+            }
+        }
+    }
+</style>
---
+++ src/components/generic/Accordion.svelte
@@ -0,0 +1,223 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+
+    export let title: string;
+    export let startOpen = false;
+    export let preventCloseOnClick = false;
+    
+    const dispatch = createEventDispatcher();
+
+    let detailsEl: HTMLDetailsElement;
+    let summaryEl: HTMLElement;
+    let contentEl: HTMLDivElement;
+    let isOpen: boolean = startOpen;
+
+    let isClosing: boolean = false;
+    let isExpanding: boolean = false;
+    let animation: Animation | null = null;
+
+    const onAnimationFinish = (open: boolean) => {
+        // Set the open attribute based on the parameter
+        isOpen = open;
+        // Clear the stored animation
+        animation = null;
+        // Reset isClosing & isExpanding
+        isClosing = false;
+        isExpanding = false;
+        // Remove the overflow hidden and the fixed height
+        detailsEl.style.height = '';
+    }
+
+    function shrink() {
+        // Set the element as "being closed"
+        isClosing = true;
+
+        // Store the current height of the element
+        const startHeight = `${detailsEl.offsetHeight}px`;
+        // Calculate the height of the summary
+        const endHeight = `${summaryEl.offsetHeight}px`;
+
+        // If there is already an animation running
+        if (animation) {
+            // Cancel the current animation
+            animation.cancel();
+        }
+
+        // Start a WAAPI animation
+        animation = detailsEl.animate({
+            // Set the keyframes from the startHeight to endHeight
+            height: [startHeight, endHeight]
+        }, {
+            // If the duration is too slow or fast, you can change it here
+            duration: 400,
+            // You can also change the ease of the animation
+            easing: 'ease-out'
+        });
+
+        // When the animation is complete, call onAnimationFinish()
+        animation.onfinish = () => onAnimationFinish(false);
+        // If the animation is cancelled, isClosing variable is set to false
+        animation.oncancel = () => isClosing = false;
+    }
+
+    function open() {
+        // Apply a fixed height on the element
+        detailsEl.style.height = `${detailsEl.offsetHeight}px`;
+        // Force the [open] attribute on the details element
+        isOpen = true;
+        // Wait for the next frame to call the expand function
+        window.requestAnimationFrame(() => expand());
+    }
+
+    function expand() {
+        // Set the element as "being expanding"
+        isExpanding = true;
+        // Get the current fixed height of the element
+        const startHeight = `${detailsEl.offsetHeight}px`;
+        // Calculate the open height of the element (summary height + content height)
+        const endHeight = `${summaryEl.offsetHeight + contentEl.offsetHeight}px`;
+
+        // If there is already an animation running
+        if (animation) {
+            // Cancel the current animation
+            animation.cancel();
+        }
+
+        // Start a WAAPI animation
+        animation = detailsEl.animate({
+            // Set the keyframes from the startHeight to endHeight
+            height: [startHeight, endHeight],
+        }, {
+            // If the duration is too slow of fast, you can change it here
+            duration: 400,
+            // You can also change the ease of the animation
+            easing: 'ease-out'
+        });
+        // When the animation is complete, call onAnimationFinish()
+        animation.onfinish = () => onAnimationFinish(true);
+        // If the animation is cancelled, isExpanding variable is set to false
+        animation.oncancel = () => isExpanding = false;
+    }
+
+    function handleToggle(e: Event) {
+        // Add an overflow on the <details> to avoid content overflowing
+        detailsEl.style.overflow = 'hidden';
+        // Check if the element is being closed or is already closed
+        if (isClosing || !isOpen) {
+            open();
+        // Check if the element is being openned or is already open
+        } else if (isExpanding || isOpen) {
+            shrink();
+        }
+    }
+
+    const onClick = (e: Event) => {
+        // Stop default behaviour from the browser
+        e.preventDefault();
+        
+        if (!detailsEl) {
+            return;
+        }
+
+        dispatch('click', open);
+
+        (
+            !preventCloseOnClick ||
+            (preventCloseOnClick && !isOpen)
+        ) && handleToggle(e);
+    }
+
+    export function Close() {
+        detailsEl.style.overflow = 'hidden';
+        shrink();
+    }
+</script>
+
+<details 
+    bind:this={detailsEl}
+    data-title={title} 
+    data-prevent-close={preventCloseOnClick}
+    open={isOpen}
+>
+    <summary bind:this={summaryEl} on:click={onClick}>
+        <div class="icon"/>{title}
+    </summary>
+    <div bind:this={contentEl} class="content">
+        <slot></slot>
+    </div>
+</details>
+
+<style lang="scss">
+    details {
+        cursor: pointer;
+        
+        --border-color: color-mix(in srgb, var(--color-green) 30%, transparent 80%);
+        border-bottom: var(--border-color) 1px solid;
+        
+        &[open] {
+            --border-color: color-mix(in srgb, var(--color-green) 70%, transparent 30%);
+
+            &[data-prevent-close] {
+                cursor: initial;
+            }
+
+            summary {
+                border-bottom: var(--border-color) 1px solid;
+
+                .icon:before, .icon:after {
+                    rotate: 0deg;
+                }
+                .icon:after {
+                    opacity: 0;
+                }
+            }
+        }
+
+        summary {
+            display: inline-flex;
+            align-items: center;
+            gap: var(--space-tiny);
+
+            width: 100%;
+
+            padding: var(--space-tiny) 0.25rem;
+
+            .icon {
+                display: block;
+                width: 1em;
+                aspect-ratio: 1/1;
+                position: relative;
+
+                border: var(--color-green) 1px solid;
+                border-radius: 100%;
+
+                &:before, &:after {
+                    content: "";
+                    display: block;
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    translate: -50% -50%;
+
+                    width: 80%;
+                    height: 2px;
+                    border-radius: 1px;
+                    background-color: var(--color-green);
+                    opacity: 1;
+                    transition: all 500ms ease-out;
+                }
+
+                &:after {
+                    rotate: 90deg;
+                }
+                &:before {
+                    rotate: 180deg;
+                }
+            }
+        }
+    }
+
+    details summary::marker {
+        content: none;
+    }
+</style>
